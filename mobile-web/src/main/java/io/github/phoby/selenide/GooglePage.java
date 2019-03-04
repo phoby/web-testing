d705d21892f4ef09ee0a449aa38f416c2c39db12
phoby <ilovezulie@gmail.com>
2019-03-05 01:11:16 +0900

    First commit

---
+++ mobile-web/src/main/java/io/github/phoby/selenide/GooglePage.java
@@ -0,0 +1,4 @@
+package io.github.phoby.selenide;
+
+public class GooglePage {
+}
---
+++ mobile-web/src/test/java/io/github/phoby/selenide/GooglePageTest.java
@@ -0,0 +1,11 @@
+package io.github.phoby.selenide;
+
+import org.junit.Test;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GooglePageTest {
+   
+   public void example() {
+      open("https://www.google.com");
+   }
+}
